a7e6ef72781377ca36279bda7612dba06802b74d
Anna <anna.manaeva@icloud.com>
2024-07-16 17:08:09 +0300

    two

--- js.js
+++ js.js
@@ -1,86 +1,111 @@
 ///////ЗАДАЧА 2
 
-let fruits = ['апельсин', 'аскорбин', 'мандарин', 'дыня'];
+// let fruits = ['апельсин', 'аскорбин', 'мандарин', 'дыня'];
 
-function summer(one, two) {
-  return (one.at(0) == two.at(0) && one.at(-1) == two.at(-1));
+// function summer(one, two) {
+//   return (one.at(0) == two.at(0) && one.at(-1) == two.at(-1));
+// }
+
+// function solution(arr) {
+//   let twoFruits = [];
+//   for (let i = 0; i < arr.length-1; i++) {
+//     if (summer(arr[i], arr[i + 1])) {
+//       twoFruits.push('true');
+//     } else {
+//       twoFruits.push('false');
+//     }
+//   }
+//   return twoFruits;
+// }
+
+// console.log(solution(fruits));
+
+//////ЗАДАЧА 1
+
+function pow(keys){
+  if(Array.isArray(keys)){
+    return keys.map(item => pow(item));
+  }else if(typeof keys === 'object'){
+    let tasks = {};
+for(let key in keys){
+  tasks[key] = pow(keys[key]);
+}
+return tasks;
+}else{
+return keys;
+}
 }
 
-function solution(arr) {
-  let twoFruits = [];
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (summer(arr[i], arr[i + 1])) {
-      twoFruits.push('true');
-    } else {
-      twoFruits.push('false');
+const obj = { a: 1, b: [2, { c: 3 }] };
+console.log((pow(obj)));
+
+
+
+
+
+// const recursiveFunc = (data) => {
+
+//   if (Array.isArray(data)) {
+//   return data.map(item => recursiveFunc(item));
+  
+//   } else if (typeof data === 'object') {
+//   const newObj = {};
+//   for (const key in data) {
+//   newObj[key] = recursiveFunc(data[key]);
+//   }
+//   return newObj;
+//   } else {
+//   return data;
+//   }
+//   }
+  
+//   const object = {a: 1, b: [2, 3, { d: 4 }], c: { e: 5, f: [6, 7] }, d: null};
+  
+//   // const temp = recursiveFunc(object)
+//   // temp.a = [1, 2]
+//   // console.log(temp)
+//   console.log(object)
+//   console.log((recursiveFunc(object)));
+
+///////////////////ЗАДАЧА 3
+const w = 3;//ширина
+const h = 3;//высота
+
+function matrix(w, h){
+    let result = new Array(h).fill().map(() => new Array(w).fill(""));
+    let couter = 1;//счетчик
+    let startCol = 0;//начинаем с 0 заполнять оbj
+    let endCol = w - 1;//конец первого массива ,-1 для того чтобы получилась ширина в 3 элетента с 0 индексом а не 4 элемента
+    let startRow = 0;//
+    let endRow = h - 1;//для того чтобы не изменить первый элемент не пошел на 1
+
+    while(startCol <= endCol && startRow <= endRow) {//первый массив          startRow   >        endCol
+        for(let i = startCol; i <= endCol; i++){
+            result[startRow][i] = couter;          //                          startCol      <      endRow
+            couter++;
+        }
+        startRow++;
+
+        for(let i = startRow; i <= endRow; i++){
+            result[i][endCol] = couter;
+            couter++;
+        }
+        endCol--;
+
+        for(let i = endCol; i >= startCol; i--){
+            result[endRow][i] = couter;
+            couter++;
+        }
+        endRow--;
+
+        for(let i = endRow; i >= startRow; i--){
+            result[i][startCol] = couter;
+            couter++;
+        }
+        startCol++;
     }
-  }
-  return twoFruits;
+
+    return result;
 }
-
-console.log(solution(fruits));
-
-////////ЗАДАЧА 1
-// function pow(keys){
-// for(let key in keys){
-//     if(typeof keys[key] === 'object' && keys[key] !== null){
-//         pow(keys[key]);
-//     }else{
-//         console.log(`${key}:${keys[key]}`);
-//     }
-// }
-// }
-// let tasks = {};
-// tasks = pow(name); 
-
-// tasks = function(obj) {
-//     console.log(obj);
-// };
-
-// pow({ a: 1, b: [2, { c: 3 }] });
-
-
-// const w = +prompt('Ширина матрицы');//трансформируем данные из строки в число +
-// const h = +prompt('Высота матрицы');
-
-// const w = 3;
-// const h = 3;
-
-// function matrix(w, h){
-//     let result = new Array(h).fill().map(() => new Array(w).fill(""));
-//     let couter = 1;
-//     let startCol = 0;
-//     let endCol = w - 1;
-//     let startRow = 0;
-//     let endRow = h - 1;
-
-//     while(startCol <= endCol && startRow <= endRow) {
-//         for(let i = startCol; i <= endCol; i++){
-//             result[startRow][i] = couter;
-//             couter++;
-//         }
-//         startRow++;
-
-//         for(let j = startRow; j <= endRow; j++){
-//             result[j][endCol] = couter;
-//             couter++;
-//         }
-//         endCol--;
-
-//         for(let i = endCol; i >= startCol; i--){
-//             result[endRow][i] = couter;
-//             couter++;
-//         }
-//         endRow--;
-
-//         for(let i = endRow; i >= startRow; i--){
-//             result[i][startCol] = couter;
-//             couter++;
-//         }
-//         startCol++;
-//     }
-
-//     return result;
-// }
-// console.log(matrix(w, h));
+console.log(matrix(w, h));
 
--- script.js
+++ script.js
@@ -1,8 +1,84 @@
-const w = 3;
-const h = 3;
+// let name = 'Anna';
+// console.log((`Hello ${name}`));
 
-function matrix(w, h){
-let result = Array.from({ length: w },() => Array(w).fill(0));
-  return result;
+// let user = prompt('whot is you name?');
+// console.log(`my name is ${user}`);
+
+// let value = true;
+// console.log(typeof value);
+
+// value = Number(value);
+// console.log(typeof value);
+
+// console.log('6' / '3');
+
+// let num = '458';
+// num = Number(num);
+// console.log(typeof num);
+
+// let x = 5, y = 3;
+// console.log(x - y);
+
+// console.log(5 % 2);
+
+// console.log(5 ** 2);
+
+// let string = 'моя';
+// console.log(typeof string);
+
+// let cum = 2;
+// cum++;
+// console.log(cum);
+
+// let a = Number(prompt("Первое число?", 1));
+// let b = Number(prompt("Первое число?", 1));
+
+// console.log(a + b);
+
+// let result = 5 == 4;
+// console.log(result);
+
+// console.log('2' > 1);
+// console.log('01' == 1);
+
+// let name = +prompt('введи число', '');
+// if(name >  0){
+//   console.log(0);
+// }else if(name < 0){
+//   console.log(-1);
+// }else if(name == 0){
+//   console.log(0);
+// }
+
+
+// let value = prompt('Введите число', 0);
+
+// if (value > 0) {
+//   console.log( 1 );
+// } else if (value < 0) {
+//   console.log( -1 );
+// } else {
+//   console.log( 0 );
+// }
+ 
+
+// let user =[{name:'Anna',age:20,}];
+// // for(let i = 0;i < user.length;i++){
+// //   console.log(user[i]);
+// // }
+
+// user.forEach(function(person, index, arr){
+//   console.log(person);
+//   console.log(index);
+
+
+// })
+
+let user = {
+  name:'dima',
+  age:28,
 }
-console.log(matrix(w, h));
+for(let key in user){
+  console.log(key);
+  console.log(user[key]);
+}
